fbc1fcbc437433b387485b5fbd92445f2e5e83dd
sashaem <mm-56@bk.ru>
2025-06-16 16:24:24 +0300

    fix pkg/

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/sashaem1/GolangPractices/pkg/inf_slices"
+	"github.com/sashaem1/GolangPractices/pkg"
 )
 
 func main() {
 	slice1 := []int{1, 2, 3}
 	slice2 := []int{4, 5, 6, 7}
-	test1 := inf_slices.CombineSlices(slice1, slice2)
+	test1 := pkg.CombineSlices(slice1, slice2)
 	fmt.Println(test1)
 }
